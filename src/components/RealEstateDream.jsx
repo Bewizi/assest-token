8ef3b6c311730f41f28eaec50cceba57db5db63b
Toluwalase <toluwalase.bewaji@yahoo.com>
2023-09-21 04:52:52 +0100

    New-updates

--- src/components/RealEstateDream.jsx
+++ src/components/RealEstateDream.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const RealEstateDream = () => {
   return (
 
@@ -11,33 +10,6 @@ const RealEstateDream = () => {
         <img src="images/location-of-land.png" alt="" />
       </div>
       <div className="achieve">
-=======
-import { useEffect } from "react";
-import AOS from "aos";
-
-
-const RealEstateDream = () => {
-  useEffect(() => {
-    AOS.init({ duration: 2000 });
-  }, []);
-  return (
-    // container of AssetToken combines the power of blockchain technology and the real estate market
-    <div className="your-dream-house">
-      {/* div containing two images,  two paragraphs, and h4 and ul  */}
-      <div className="garden-location">
-        <img
-          src="images/beautiful-garden-around-house.png"
-          alt="Beautiful Garden Around House"
-          data-aos="fade-up-right"
-        />
-        <img
-          src="images/location-of-land.png"
-          alt="Location of Land"
-          data-aos="fade-up-left"
-        />
-      </div>
-      <div className="achieve" data-aos="flip-right">
->>>>>>> f4db1a7 (new-update)
         <p>We Help You Achieve Your</p>
         <p>Real Estate Dreams</p>
       </div>
@@ -63,12 +35,8 @@ const RealEstateDream = () => {
           </li>
         </ul>
       </div>
-<<<<<<< HEAD
     {/* end of div containing two images,  two paragraphs, and h4 and ul  */}
 
-=======
-      {/* end of div containing two images,  two paragraphs, and h4 and ul  */}
->>>>>>> f4db1a7 (new-update)
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-<<<<<<< HEAD
-=======
-import "aos/dist/aos.css";
-
->>>>>>> f4db1a7 (new-update)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
